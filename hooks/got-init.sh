4fdb8483a17d21f358e0277bba85105bdf57daac
Hassan Kandil <hassanosama@aucegypt.edu>
2020-05-01 05:01:27 +0200

    last working version before progress presentation IV

--- hooks/got-init.sh
+++ hooks/got-init.sh
@@ -1,14 +1,20 @@
+echo "Initializing GoT Repository..."
+
 git init
 git --bare init .got
 git remote add origin .got
 git remote set-url origin .got
+# git push --set-upstream origin master
+# git push --set-downstream origin master
 mkdir .gotconfig
 
-#adding wallet info
+# echo ".got \n .gotconfig"  >> .gitignore
+
+echo "Adding wallet info..."
 cp -r $GOPATH/src/github.com/GitofTruth/GoT/profiles .gotconfig
 
 #creating wallet
 sudo got-wallet add acme Admin
 
 
-rm -rf .go/objects
+# rm -rf .go/objects
