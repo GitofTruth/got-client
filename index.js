bb60307cdbb84ab95c37d3ded72b9458915cb5d1
Muhammad Faisal <mfaisal@aucegypt.edu>
2020-05-02 22:50:02 +0200

    stable - Access Control logic implemented - remaining enc functions

--- index.js
+++ index.js
@@ -44,9 +44,7 @@ async function main(){
         await client.addRepo();
         break;
       
-      case "test":
-        console.log("Test zy el fol");
-        
+      case "testUser":
         // testing user update messages
         console.log("Testing User Creation")
         await client.createNewUser("mickey", "PublickKey", "PrivateKey")
@@ -72,6 +70,64 @@ async function main(){
         await client.queryUserPublicKey("NewMickey")
         console.log("\n\n")
 
+        break;
+
+      
+      case "testAccess":
+        // testing user update messages
+        console.log("Testing Adding Users and Repo")
+        await client.createNewUser("User01", "PP", "PP")
+        await client.createNewUser("User02", "PP", "PP")
+        // await client.createNewUser("User03", "PP", "PP")
+        // await client.createNewUser("User04", "PP", "PP")
+        // await client.createNewUser("User05", "PP", "PP")
+        await client.createNewUser("Admin01", "PP", "PP")
+        await client.createNewUser("Admin02", "PP", "PP")
+        // await client.createNewUser("Admin03", "PP", "PP")
+        // await client.createNewUser("Admin04", "PP", "PP")
+        // await client.createNewUser("Admin05", "PP", "PP")
+        await client.createNewUser("Admin@acme.com", "PP", "PP")
+        await client.addRepo()
+        await client.queryCurrentEncryptionKey()
+        await client.queryUsersAccess()
+        console.log("\n\n")
+
+        // A01
+        // U02
+        console.log("Adding Collaborators")
+        await client.updateUserAccess("Admin01", gotClient.UserAccess.CollaboratorAccess, "enc2")
+        await client.createNewUser("Admin01", "PP", "PP", false)
+        await client.updateUserAccess("User02", gotClient.UserAccess.ReadWriteAccess, "enc2")
+        await client.updateUserAccess("Admin02", gotClient.UserAccess.CollaboratorAccess, "enc2")
+        await client.queryCurrentEncryptionKey()
+        await client.queryUsersAccess()
+        console.log("\n\n")
+
+        // // A01, 
+        // // U02, U01,
+        // console.log("Adding Read/Write")
+        // await client.createNewUser("Admin@acme.com", "PP", "PP", false)
+        // await client.updateUserAccess("User01", gotClient.UserAccess.ReadWriteAccess, "enc3")
+        // await client.createNewUser("User01", "PP", "PP", false)
+        // await client.updateUserAccess("User03", gotClient.UserAccess.ReadWriteAccess, "enc3")
+        // await client.updateUserAccess("Admin03", gotClient.UserAccess.CollaboratorAccess, "enc3")
+        // await client.queryCurrentEncryptionKey()
+        // await client.queryUsersAccess()
+        // console.log("\n\n")
+
+        // // A01, A04, A05
+        // // U02, U01, U04, U05
+        // console.log("Changing Ownership")
+        // await client.createNewUser("Admin@acme.com", "PP", "PP", false)
+        // await client.updateUserAccess("Admin01", gotClient.UserAccess.OwnerAccess, "enc4")
+        // await client.updateUserAccess("User04", gotClient.UserAccess.ReadWriteAccess, "enc4")
+        // await client.updateUserAccess("Admin04", gotClient.UserAccess.CollaboratorAccess, "enc4")
+        // await client.createNewUser("Admin01", "PP", "PP", false)
+        // await client.updateUserAccess("User05", gotClient.UserAccess.ReadWriteAccess, "enc5")
+        // await client.updateUserAccess("Admin05", gotClient.UserAccess.CollaboratorAccess, "enc5")
+        // await client.queryCurrentEncryptionKey()
+        // await client.queryUsersAccess()
+        // console.log("\n\n")
 
         break;
   
