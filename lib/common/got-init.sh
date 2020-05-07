411a33e2be7f54e9566131393c421f562ddac966
Hassan Kandil <hassanosama@aucegypt.edu>
2020-05-07 12:17:41 +0200

    Working got-client integration with deployed fabric

--- lib/common/got-init.sh
+++ lib/common/got-init.sh
@@ -10,7 +10,11 @@ git remote set-url origin .got/remote
 
 #adding wallet info
 mkdir -p .got/config/hyperledger
-cp -r $GOPATH/src/github.com/GitofTruth/GoT/profiles .got/config/hyperledger
+# cp -r $GOPATH/src/github.com/GitofTruth/GoT/profiles .got/config/hyperledger
+wget -r -nH -q -P ./.got/config/hyperledger -R "index.html*" http://13.95.171.119/profiles/
+# wget -r -nH -q -P ./.got/config/hyperledger -R "index.html*" http://13.95.171.119/crypto-config/
+wget -r -nH -q -P ./.got/config/hyperledger -R "index.html*" http://13.95.171.119/crypto-config.zip
+unzip -q ./.got/config/hyperledger/crypto-config.zip -d ./.got/config/hyperledger
 
 #creating wallet
 got-wallet add acme Admin
